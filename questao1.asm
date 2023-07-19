3bbc4ea6dffd0384d166efdd814312dd73062c5e
mari g <113149415+marigmsc@users.noreply.github.com>
2023-07-19 15:02:35 -0300

    Update questao1.asm

--- questao1.asm
+++ questao1.asm
@@ -8,29 +8,26 @@ lw x15, zero
 
 lw x8,x
 # a >= 0
-bge x10,x15,greaterthanzero
+bge x10,x15,greaterzero
 blt x10,x15,done
-# b >= 62
-greaterthanzero:
-bge x11,x13,greaterthansixtytwo
-blt x11,x13,done
+# b <= 62
+greaterzero:
+beq x11,x13,lowerequalsixtytwo
+blt x11,x13,lowerequalsixtytwo
 #c < 15
-greaterthansixtytwo:
-blt x12,x14,lowerthanfifteen
+lowerequalsixtytwo:
+blt x12,x14,lowerfifteen
 bge x12,x14,done
 
-lowerthanfifteen:
+lowerfifteen:
 addi  x8,x8,1
-
-
+done:
 halt
-a: .word 2
-b: .word 65
-c: .word 14
+a: .word 0
+b: .word 62
+c: .word 13
 x : .word 0
 sixtytwo: .word 62
 fifteen: .word 15
-zero: .word 0
-
-zero: .word 0
+zero: .half 0
 
