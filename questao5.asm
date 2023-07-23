b8ff9c5400663dad59ad042f500c330d707513c7
nogueira04 <gabrielnogueira4014@gmail.com>
2023-07-23 16:09:31 -0300

    q5 input do primeiro numero funcionando

--- questao5.asm
+++ questao5.asm
@@ -12,39 +12,58 @@ lw x15, mult_index
 lw x16, number_a
 lw x17, input_flag
 lw x18, ten
+lw x19, receive_loop_index
+lw x20, two
+lw x21, three
+lw x22, four
 
 receive_number:
+    addi x19, x19, 1
     lw x12, 1025(x0)
-    beq x12, x13, end_input     # término do input no char de ponto final
-    #beq x12, x14, prep_next_num # preparo dos registradores para receber o próx número 
+    beq x12, x13, end_input
+	addi x12, x12, -48
 
-    sw x9, product
-    lw x15, product # arrumar
-
-    sw x12, multiplicand
-    sw x15, multiplier
-    lw x5, multiplicand
-    lw x6, multiplier
-
-    xor x17, x17, x17
-    addi x17, x17, 1
-    xor x10, x10, x10
-    jal ra, multiply
-
-    next_step:
-        add x16, x16, x9
-        xor x9, x9, x9
-
-        sw x15, multiplicand
-        sw x18, multiplier
+	xor x15, x15, x15
+    beq x19, x8, set_1000
+    beq x19, x20, set_100
+    beq x19, x21, set_10
+    beq x19, x22, set_1
+    
+    set_1000:
+        addi x15, x15, 1000
+        jal ra, continue_receiving
+    
+    set_100:
+        addi x15, x15, 100
+        jal ra, continue_receiving
+    
+    set_10:
+        addi x15, x15, 10
+        jal ra, continue_receiving
+    
+    set_1:
+        addi x15, x15, 1
+        
+    
+    continue_receiving:
+        sw x12, multiplicand
+        sw x15, multiplier
         lw x5, multiplicand
         lw x6, multiplier
 
         xor x17, x17, x17
+        addi x17, x17, 1
         xor x10, x10, x10
-        addi x17, x17, 10
+        xor x9, x9, x9
         jal ra, multiply
 
+    next_step:
+        add x16, x16, x9
+        xor x9, x9, x9
+        xor x17, x17, x17
+        xor x10, x10, x10
+        addi x17, x17, 10
+        jal ra, receive_number
         
 
     end_input:
@@ -70,7 +89,7 @@ multiply:
         jal ra, multiply
 
 control:
-    beq x17, x0, not_input
+    beq x17, x0, end
     beq x17, x8, next_step
     beq x17, x18, receive_number
 
@@ -80,15 +99,20 @@ end:
 
 multiplicand: .word 0
 multiplier: .word 0
-product: .word 1
+product: .word 0
 multiplier0: .word 0
 one: .word 1
+two: .word 2
+three: .word 3
+four: .word 4
 ten: .word 10
 loop_index: .word 0
 loop_limit: .word 32
 char_num: .word 0
 space_char: .word 32
 point_char: .word 46
-mult_index: .word 1
+mult_index: .word 0
 number_a: .word 0
 input_flag: .word 1
+receive_loop_index: .word 0
+
